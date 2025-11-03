ce5c5a08d7c765dca4d3737a68dee2c6938ab468
Ashraf Zachry <ashrafzachry.azrin@student.gmi.edu.my>
2025-11-04 00:56:00 +0800

    Replace old files with new NTAVisWebsite version

--- index_page.py
+++
@@ -1,8 +0,0 @@
-# index_page.py
-import streamlit as st
-import streamlit.components.v1 as components
-
-with open("index.html", "r") as f:
-    html_content = f.read()
-
-components.html(html_content, height=800, scrolling=True)
